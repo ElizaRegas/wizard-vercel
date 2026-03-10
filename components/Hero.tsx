8070fb12f9764242596ad63602b927a51a6911e1
Elizabeth Regas <elizaregas@gmail.com>
2026-03-10 09:35:09 -0400

    Initial portfolio app setup

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+        <section>
+            <p>Portfolio</p>
+            <h1>Elizabeth Regas</h1>
+            <p>
+                Designer, developer, musician, and creative strategist building
+                elegant digital experiences.
+            </p>
+        </section>
+    );
+}
